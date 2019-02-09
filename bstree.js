54be4b30db7bb154ac8e3287bd6a77ddb81ca739
David Seidenberg <davidseidenberg@Davids-MBP.attlocal.net>
2019-02-09 14:45:50 -0800

    Left Rotation

--- bstree.js
+++ bstree.js
@@ -5,28 +5,94 @@
 class Node {
   constructor(data) {
     this.data = data;
+    this.height = 1;
     this.left = null;
     this.right = null;
   }
 }
 
-class BSTree {
+class AVLTree {
   constructor() {
     this.root = null;
   }
 
+  getHeight(node) {
+    if (node === null) {
+      return 0;
+    }
+    return node.height;
+  }
+
+  rightRotate(node) {
+    
+  }
+
+  leftRotate(node) {
+
+  }
+
+  getBalance(node) {
+    if (node === null) {
+      return 0;
+    }
+    return this.getHeight(node.left) - this.getHeight(node.right);
+  }
+
+  rebalance(node) {
+    let balance = this.getBalance(node);
+    console.log(`Balance of node ${node.data} is ${balance}`);
+
+    if (Math.abs(balance) <= 1) return;
+
+
+    // // left left
+    if (balance > 1 && this.getBalance(node.left) > 0) {
+      console.log('left left');
+
+    }
+
+    // // left right
+    if (balance > 1 && this.getBalance(node.left) < 0) {
+      console.log('left right');
+    }
+    
+    // // right right
+    if (balance < -1 && this.getBalance(node.right) < 0) {
+      console.log('right right');
+      // rotate node left
+      let right = node.right;
+      let child = right.left;
+
+      right.left = node;
+      node.right = child;
+
+      this.updateHeight(node);
+      this.updateHeight(right);
+      this.root = right;
+      return;
+    }
+
+        // // right left
+    if (balance < -1 && this.getBalance(node.right) > 0) {
+      console.log('left left')
+    }
+  }
+
   search(node, data) {
     if (node === null) return null;
-
     if (data < node.data) {
       return this.search(node.left, data);
     }
-    
     if (data > node.data) {
       return this.search(node.right, data);
     }
-
     return node;
+  }
+
+  updateHeight(node) {
+    let leftHeight = this.getHeight(node.left);
+    let rightHeight = this.getHeight(node.right);
+    node.height = 1 + Math.max(leftHeight, rightHeight);
   }
 
   insert(data) {
@@ -34,30 +100,31 @@ class BSTree {
 
     if (this.root === null) {
       this.root = newNode;
-      return;
+    } else {
+      this.insertNode(this.root, newNode);
     }
-
-    this.insertNode(this.root, newNode);
   }
 
   insertNode(node, newNode) {
+
     if (newNode.data < node.data) {
       if (!node.left) {
         node.left = newNode;
-        return;
+      } else {
+        this.insertNode(node.left, newNode);
       }
-      this.insertNode(node.left, newNode);
-      return;
     }
 
     if (newNode.data > node.data) {
       if (!node.right) {
         node.right = newNode;
-        return;
-      } 
-      this.insertNode(node.right, newNode);
-      return;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
     }
+
+    this.updateHeight(node);   
+    this.rebalance(node);
   }
 
   remove(data) {
@@ -112,163 +179,35 @@ class BSTree {
   }
 
   inOrder(node) {
-
+    if (node !== null) {
+      this.inOrder(node.left);
+      console.log(node.data);
+      this.inOrder(node.right);
+    }
   }
 
   preOrder(node) {
-
+    if (node !== null) {
+      console.log(node.data);
+      this.preOrder(node.left);
+      this.preOrder(node.right);
+    }
   }
 
   postOrder(node) {
-
+    if (node !== null) {
+      this.postOrder(node.left);
+      this.postOrder(node.right);
+      console.log(node.data);
+    }
   }
 }
 
-
-
-/*
-        5
-      2   6
-    1  4    7 
-*/
-
-const myTree = new BSTree();
+const myTree = new AVLTree();
 myTree.insert(5);
-myTree.insert(2);
 myTree.insert(4);
-myTree.insert(6);
-myTree.insert(1);
 myTree.insert(7);
-myTree.remove(5);
+myTree.insert(9);
+myTree.insert(6);
+myTree.insert(8);
 console.log(myTree.root);
-
-
-
-
-// class BSTree {
-//   constructor(val) {
-//     this.val = val;
-//     this.left = null;
-//     this.right = null;
-//   }
-
-//   insert(val) {
-//     if (val < this.val) {
-
-//       if (this.left === null) {
-//         this.left = new BSTree(val);
-//         return;
-//       }
-//       this.left.insert(val);
-//       return;
-//     }
-
-//     if (val > this.val) {
-//       if (this.right === null) {
-//         this.right = new BSTree(val);
-//         return;
-//       } 
-//       this.right.insert(val);
-//       return;
-//     }
-//   }
-
-//   search(val) {
-//     // search is straightforward
-//     if (this.val === val) return this;
-
-
-//     if (val > this.val) {
-//       if (!this.right) return -1;
-//       return this.right.search(val);
-//     }
-
-
-//     if (val < this.val) {
-//       if (!this.left) return -1;
-//       return this.left.search(val);
-//     }
-//   }
-
-//   remove(val) {
-
-//     console.log(`Starting from ${JSON.stringify(this)}`);
-//     console.log(`Removing ${val}`);
-
-//     let node = this.search(val);
-//     let parent = this._findParentOfNode(val);
-
-//     console.log(`Node is ${JSON.stringify(node)}`);
-//     console.log(`Parent is ${JSON.stringify(parent)}`);
-
-
-//     if (!node.left && !node.right) {
-//       if (parent.right && val === parent.right.val) {
-//         parent.right = null;
-//       }
-
-//       if (parent.left && val === parent.left.val) {
-//         parent.left = null;
-//       }
-//     }
-
-//     if (node.left && node.right) {
-//       let smallest = node.right._findSmallest();
-//       console.log(`smallest is ${JSON.stringify(smallest)}`);
-
-//       node.val = smallest.val;
-//       node.right.remove(smallest.val);
-//     }
-
-
-//     if (node.left) {
-//       if (parent && val === parent.right) {
-//         parent.right = node.left;
-//       }
-//       if (parent && val === parent.left) {
-//         parent.left = node.left;
-//       }
-//     }
-
-//     if (node.right) {
-//       if (parent && val === parent.right) {
-//         parent.right = node.right;
-//       }
-//       if (parent && val === parent.left) {
-//         parent.left = node.right;
-//       }
-//     }
-//   }
-
-
-//   _findParentOfNode(val) {
-//     if (this.val === val) {
-//       return this;
-//     }
-
-//     if (val < this.val) {
-//       if (this.left.val === val) return this;
-//       return this.left._findParentOfNode(val);
-//     }
-
-//     if (val > this.val) {
-//       if (this.right.val === val) return this;
-//       return this.right._findParentOfNode(val);
-//     }
-//   }
-
-//   _findSmallest() {
-//     console.log(`This in find smallest is ${JSON.stringify(this)}`);
-//     if (this.left === null) return this;
-
-//     return this.left._findSmallest();
-//   }
-
-//   _findKthSmallest(k) {
-
-//   }
-
-//   _balance() {
-
-//   }
-// }
